08e18fe93098291576eb080f0314da87829fbc87
muddin1121 <muddin1121@gmail.com>
2021-10-12 15:56:58 -0400

    completed first part of render function and changed our queryselector from an array

--- js/app.js
+++ js/app.js
@@ -1,11 +1,7 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
-let currentPlayer = 1
-board = [
-  null, null, null,
-  null, null, null,
-  null, null, null,
-]
+let turn
+let board = []
 
 let isWinner
 
@@ -13,9 +9,9 @@ let isWinner
 
 /*-------------------------------- Constants --------------------------------*/
 
-const winningMessage = `Player ${currentPlayer}`
+const winningMessage = `Player ${turn}`
 const drawMessage = `Game is a DRAW!`
-const currentPlayerTurn = `It is currently Player ${currentPlayer} turn.`
+const currentPlayerTurn = `It is currently Player ${turn} turn.`
 
 
 
@@ -23,15 +19,9 @@ const currentPlayerTurn = `It is currently Player ${currentPlayer} turn.`
 
 
 /*------------------------ Cached Element References ------------------------*/
-const square1 = document.getElementById('sq0')
-const square2 = document.getElementById('sq1')
-const square3 = document.getElementById('sq2')
-const square4 = document.getElementById('sq3')
-const square5 = document.getElementById('sq4')
-const square6 = document.getElementById('sq5')
-const square7 = document.getElementById('sq6')
-const square8 = document.getElementById('sq7')
-const square9 = document.getElementById('sq8')
+
+let squares = document.querySelectorAll('#sq')
+
 const gameStatus = document.getElementById('message')
 
 
@@ -41,20 +31,33 @@ const gameStatus = document.getElementById('message')
 
 
 /*-------------------------------- Functions --------------------------------*/
-
+init()
 function init(){
   board = [
-    square1, square2, square3,
-    square4, square5, square6,
-    square7, square8, square9,
+    null, null, null,
+    null, null, null,
+    null, null, null,
   ]
   isWinner = null;
-  let currentPlayer = 1
+  turn = 1
   render ()
 
   
 }
 
 function render(){
+  squares.forEach(function(square, idx){
+    board[idx]= square
+    if (turn === 1 ){ //& element is clicked
+      square.style.backgroundColor = 'green'
+    }
+    else if (turn === -1 ){ //& element is clicked
+      square.style.backgroundColor = 'yellow'
+    }
+  })
 
-}
+
+
+  }
+  
+
