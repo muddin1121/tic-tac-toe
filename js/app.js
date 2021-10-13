038e8983cb889f2e4336c2212d8bb71ca8ec5195
muddin1121 <muddin1121@gmail.com>
2021-10-12 22:45:39 -0400

    completed the get winner function

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 /*---------------------------- Variables (state) ----------------------------*/
 let turn
 let board = []
-
 let isWinner
+let bob 
+
 
 
 
@@ -13,7 +14,14 @@ const winningMessage = () => `Player ${turn} is the winner`
 const drawMessage = () => `Game is a DRAW!`
 const currentPlayerTurn = () => `It is currently Player ${turn} turn.`
 
-
+let winningArray =   [  [0,1,2],
+                        [3,4,5],
+                        [6,7,8],
+                        [0,3,6],
+                        [1,4,7],
+                        [2,5,8],
+                        [6,4,2],
+                        [0,4,8]   ]
 
 
 
@@ -72,27 +80,50 @@ function render(){
   }
 
   
-const winningArray = [  [0,1,2],
-                        [3,4,5],
-                        [6,7,8],
-                        [0,3,6],
-                        [1,4,7],
-                        [2,5,8],
-                        [6,4,2],
-                        [0,4,8]   ]
+
 
 function handleClick(evt){
   if(board[parseInt(evt.target.id.replace('sq',''))]!==null){
     return
   }
-board[(evt.target.id.replace("sq",""))]=turn
-turn = turn * -1
+  else if(isWinner!==null){
+    return
+  }
+  board[(evt.target.id.replace("sq",""))]=turn
+  turn = turn * -1
+
+  let winner = getWinner()
+  
+  function getWinner(){
+    winningArray.forEach(function(indexArray){
+      
+      let idx1 = indexArray[0]
+      let idx2 = indexArray[1]
+      let idx3 = indexArray[2]
+      let theWinner = board[idx1]
+      let sum = Math.abs(board[idx1] + board[idx2] + board[idx3])
+      if (sum === 3){
+        isWinner=theWinner
+        return theWinner
+
+      }
+      else if(board.includes(null)){
+        return null
+      }
+      else{
+        isWinner = "T"
+        return "T"
+      }
+      
+    })
 
 
-render()
+  
+  }
 
 
 
+  render()
 }
 
 
